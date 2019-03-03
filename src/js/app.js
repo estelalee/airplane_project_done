db4f35f7c68ae72723dee72a6789489eba8353f4
Li <mac@szcube-de-macbook-pro.local>
2019-03-04 00:03:20 +0800

    0304

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,36 @@ $('.explore__slide').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    arrows: false,
+    arrows: true,
     autoplay: false,
+
+    prevArrow: ".explore__arrow__prev",
+    nextArrow: ".explore__arrow__next",
+
+    // responsive: [{
+    //         breakpoint: 1024,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 3,
+    //             infinite: true,
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 600,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 480,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
+    //         }
+    //     }
+    // ]
+
 });
 
 
