0c7d431b3d263635e07a61c8ef796b0b7beac9e3
Li <mac@szcube-de-macbook-pro.local>
2019-03-10 23:23:49 +0800

    0310

--- src/js/app.js
+++ src/js/app.js
@@ -18,31 +18,32 @@ $('.explore__slide').slick({
     prevArrow: ".explore__arrow__prev",
     nextArrow: ".explore__arrow__next",
 
-    // responsive: [{
-    //         breakpoint: 1024,
-    //         settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 3,
-    //             infinite: true,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 600,
-    //         settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 2
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 480,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //         }
-    //     }
-    // ]
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 
 });
+
 
 
 $('.instruction__container').slick({
@@ -54,4 +55,28 @@ $('.instruction__container').slick({
 
     prevArrow: ".instruction__arrow__prev",
     nextArrow: ".instruction__arrow__next",
+
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+        }
+    },
+    {
+        breakpoint: 800,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }
+]
 });
